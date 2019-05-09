007d820038d2eff831bd04b35c5461dd8293cee3
Hoor Ahmed <hooraoa@gmail.com>
2019-05-09 23:46:16 +0300

    MongoDB connected and User Model added

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
 const PORT = process.env.PORT || 5500;
-const db = require("./config/keys").MongoURI;
 
+//MongoDB Config 
+const db = require('./config/keys').MongoURI;
+
+//MongoDB Connect
 mongoose.connect(db,{useNewUrlParser:true})
-.then(()=> console.log("MongoDB connected"))
+.then(()=> console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-app.use(bodyParser.urlencoded({extended:true}));
-// app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +'/public'));
 
+//bodyParser
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Routes
 app.use('/',require('./routes/landing'));
 app.use('/user',require('./routes/user'));
 
